0fb800fbe29b08dacf1bae82572ce80aac1c3f6c
Rafael Delanhese <rdmmail-1@yahoo.com.br>
2021-04-02 13:10:36 -0300

    Adicionando validação no lado servidor

--- src/com/hepta/funcionarios/entity/Setor.java
+++ src/com/hepta/funcionarios/entity/Setor.java
@@ -1,51 +1,47 @@
 package com.hepta.funcionarios.entity;
 
 import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Setor implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "ID_SETOR")
-	private Integer id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "ID_SETOR")
+  private Integer id;
 
-	@Column(name = "NOME")
-	private String nome;
+  @NotNull(message = "Nome não pode ser nulo")
+  @Column(name = "NOME")
+  private String nome;
 
+  public Setor() {}
 
-	public Setor() {
-	}
+  public Setor(Integer id, String nome) {
+    this.id = id;
+    this.nome = nome;
+  }
 
+  public Integer getId() {
+    return id;
+  }
 
-	public Setor(Integer id, String nome) {
-		this.id = id;
-		this.nome = nome;
-	}
-	
+  public void setId(Integer id) {
+    this.id = id;
+  }
 
-	public Integer getId() {
-		return id;
-	}
+  public String getNome() {
+    return nome;
+  }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
